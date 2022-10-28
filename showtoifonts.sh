d81cf7d2e41dd3d9d33412ea70c2bb23d37b662a
1337encrypted <smallaszero@gmail.com>
2022-10-28 19:36:26 +0530

    deleted Future.tlf

--- showtoifonts.sh
+++ showtoifonts.sh
@@ -1,6 +1,1 @@
-for f in ~/figletfonts/*
-do 
-  fs=$(basename $f)
-  fname=${fs%%.tlf}
-  toilet -f $fname $fname
-done
+var=$(ls ~/figletfonts/ | sed '/-/d' | sed 's/\..*$//g') && for i in ${var[@]}; do echo -e "\n\n$i"; figlet -f $i The quick brown fox jumps over the lazy dog; done
