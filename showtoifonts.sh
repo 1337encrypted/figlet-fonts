fbf5b59fb3e81739a09cf6d54ab50e65e878ed80
Yash Herekar <smallaszero@gmail.com>
2023-09-30 22:04:27 +0530

    Update showtoifonts.sh

--- showtoifonts.sh
+++ showtoifonts.sh
@@ -1,1 +1,1 @@
-var=$(ls ~/figletfonts/ | sed '/-/d' | sed 's/\..*$//g') && for i in ${var[@]}; do echo -e "\n\n$i"; figlet -f $i The quick brown fox jumps over the lazy dog; done
+var=$(ls /usr/share/figlet | sed '/-/d' | sed 's/\..*$//g') && for i in ${var[@]}; do echo -e "\n\n$i"; figlet -f $i The quick brown fox jumps over the lazy dog; done
