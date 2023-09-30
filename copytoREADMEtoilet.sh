50f40f596fc02b68d74e40083074c0966955a654
1337encrypted <smallaszero@gmail.com>
2023-10-01 02:52:03 +0530

    toilet colors added

--- copytoREADMEtoilet.sh
+++ copytoREADMEtoilet.sh
@@ -2,5 +2,5 @@ var=$(ls . | sed '/-/d' | sed 's/\..*$//g') &&
 	for i in ${var[@]}; do
 		echo "\n\n### $i";
 		echo "\`\`\`";
-		figlet -ckf $i -w $(tput cols) Encrypted | toilet --metal -w $(tput cols) -f term;
+		figlet -lkf $i -w $(tput cols) Encrypted | toilet --metal -w $(tput cols) -f term;
 		echo "\`\`\`"; done
