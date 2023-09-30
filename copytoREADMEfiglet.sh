e9ba470d4423526097fd11bedb7c904d61d8313e
1337encrypted <smallaszero@gmail.com>
2023-10-01 02:57:49 +0530

    final changes for the day

--- copytoREADMEfiglet.sh
+++ copytoREADMEfiglet.sh
@@ -2,5 +2,5 @@ var=$(ls . | sed '/-/d' | sed 's/\..*$//g') &&
 	for i in ${var[@]}; do
 		echo "\n\n### $i";
 		echo "\`\`\`";
-		figlet -lf $i -w 120 Encrypted;
+		figlet -lf $i -w 170 1337Encrypted;
 		echo "\`\`\`"; done
